6ba5b09f8372dcce28635e6aaa2f2711a908c0ca
Auxtnn <jonnichas@gmail.com>
2024-12-29 20:50:06 +0100

    added meta content

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,9 @@ import ToastProvider from "./components/ToastProvider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Luxville Apartments",
-  description: "A real estate and apartment listing website",
+  title: "Samchukwu Properties",
+  description:
+    "Find your ideal property with Samchukwu Properties. We offer prime land for sale and affordable apartment rentals. Start your property journey with us today.",
 };
 
 export default function RootLayout({
